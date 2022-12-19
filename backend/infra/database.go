cf6d16f68830e9ba18ad7fd13da402c5571654a6
daichinagano <d.nagano1002@gmail.com>
2022-12-19 12:03:00 +0900

    ✨ connect database

---
+++ backend/infra/database.go
@@ -0,0 +1,58 @@
+package infra
+
+import (
+	"backend/constant"
+	"backend/util"
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var logger = util.NewLogger()
+
+type GormHandler struct {
+	DB *gorm.DB
+}
+
+func NewGormHandler() *GormHandler {
+	conn, err := connectDB()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	GormHandler := new(GormHandler)
+	GormHandler.DB = conn
+	return GormHandler
+}
+
+func connectDB() (*gorm.DB, error) {
+	dbName := viper.GetString(constant.DBNameEnv)
+	dbUser := viper.GetString(constant.DBUserEnv)
+	dbHost := viper.GetString(constant.DBHostEnv)
+
+	dsn := fmt.Sprintf("%v@tcp(%s)/%v?charset=utf8&parseTime=True&loc=Local&timeout=10s", dbUser, dbHost, dbName)
+
+	var db *gorm.DB
+	var err error
+	for i := 1; i <= constant.DBConnectRetryMaxCount; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		fmt.Println(err)
+		time.Sleep(constant.RetryWaitTime * time.Millisecond)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	migrateErr := db.AutoMigrate()
+
+	if migrateErr != nil {
+		return nil, migrateErr
+	}
+	return db, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"backend/constant"
+	"backend/infra"
+	"fmt"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("server start!")
+	// 環境変数読み込み
+	loadEnv()
+	// DB初期化処理
+	gormHandler := infra.NewGormHandler()
+
+	fmt.Println(gormHandler)
+	// repos := domainrepo.Repositories{}
+
+	// router := gin.Default()
+
+	// setCors(router)
+
+	// ctx := context.Background()
+
+	// githubClient := server.NewGithubApiClient(ctx)
+
+	// server.NewApiServer(ctx, router, repos, githubClient)
+
+	// router.Run()
+	// logger.Info("server start!")
+}
+
+func setCors(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
+
+func loadEnv() {
+	viper.AutomaticEnv()
+	viper.SetDefault(constant.DBHostEnv, "db")
+	viper.SetDefault(constant.DBUserEnv, "root")
+	viper.SetDefault(constant.DBNameEnv, "web")
 }
