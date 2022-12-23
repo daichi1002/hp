5b272b09cf960497742d3a45df61dd4d5628bfd4
daichinagano <d.nagano1002@gmail.com>
2022-12-23 09:08:16 +0900

    💄 view batch status

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -31,26 +31,38 @@ const commits = await getCommits();
         <!-- Profile tab -->
         <!-- About Section -->
         <div class="bg-white p-3 border rounded-lg">
-          <div
-            class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
-          >
-            <span clas="text-green-500">
-              <svg
-                class="h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
-              </svg>
-            </span>
-            <span class="tracking-wide">Engineer Profile</span>
+          <div class="flex items-center justify-between">
+            <div
+              class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
+            >
+              <span>
+                <svg
+                  class="h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+              </span>
+              <span class="tracking-wide">Engineer Profile</span>
+            </div>
+            <div class="flex">
+              <div class="flex font-normal">
+                <div class="border-t border-l border-b p-1 text-sm">○</div>
+                <div class="p-1 mr-3 border text-sm">languages</div>
+              </div>
+              <div class="flex">
+                <div class="border-t border-l border-b p-1 text-sm">○</div>
+                <div class="mr-3 border p-1 text-sm">contributions</div>
+              </div>
+            </div>
           </div>
           <div class="text-gray-700">
             <div class="grid md:grid-cols-2 text-sm">
