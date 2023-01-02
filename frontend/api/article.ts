94d4a54a32b3a6e24bf481211cffa5c57d534e6e
daichinagano <d.nagano1002@gmail.com>
2023-01-02 15:55:03 +0900

    🔥 remove fetch article

--- frontend/api/article.ts
+++ frontend/api/article.ts
@@ -1,16 +1,4 @@
 import { Article } from "~~/types/article";
-export const listArticles = async () => {
-  const config = useRuntimeConfig();
-  const { data, error } = await useFetch<Article[]>(
-    config.public.PUBLIC_BACKEND_URL + "articles"
-  );
-
-  if (!data.value || error.value) {
-    console.log(error.value?.message);
-  }
-
-  return data;
-};
 
 export const getArticle = async (id: string | string[]) => {
   const config = useRuntimeConfig();
