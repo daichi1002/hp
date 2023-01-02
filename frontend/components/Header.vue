d6efa6b78e3c4e2682853bac6ee613aa8d19a7c5
daichinagano <d.nagano1002@gmail.com>
2023-01-02 17:29:55 +0900

    🐛 fix link name

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -7,7 +7,7 @@
         class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
       >
         <NuxtLink to="/" class="mr-5 hover:text-gray-900">Home</NuxtLink>
-        <NuxtLink to="/article" class="mr-5 hover:text-gray-900"
+        <NuxtLink to="/articles" class="mr-5 hover:text-gray-900"
           >Articles</NuxtLink
         >
       </nav>
