026e9d9d522706f74114994bee11221623fedf08
daichinagano <d.nagano1002@gmail.com>
2023-01-03 17:25:05 +0900

    🐛 fix connect database

--- backend/infra/database.go
+++ backend/infra/database.go
@@ -38,7 +38,7 @@ func connectDB() (*gorm.DB, error) {
 	if len(dbPassword) > 0 {
 		dbAuth += ":" + dbPassword
 	}
-	dsn := fmt.Sprintf("%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local&timeout=10s", dbAuth, dbHost, dbPort, dBName)
+	dsn := fmt.Sprintf("%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local&timeout=10s", dbUser, dbHost, dbPort, dBName)
 
 	var db *gorm.DB
 	var err error
