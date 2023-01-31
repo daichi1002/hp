ef79535766ea889a4e2f37752b42af2c56e20190
daichinagano <d.nagano1002@gmail.com>
2023-01-31 16:12:48 +0900

    ✨ add get article state

--- frontend/composables/article.ts
+++ frontend/composables/article.ts
@@ -24,3 +24,28 @@ const listArticles = async (state: Ref<Article[]>) => {
     state.value.push(e);
   });
 };
+
+const getArticle = async (id: string | string[]) => {
+  const config = useRuntimeConfig();
+  const url = config.public.PUBLIC_BACKEND_URL + `article/${id}`;
+
+  const res = await $fetch<Article>(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    retry: 3,
+  });
+
+  return res;
+  // TODO：上記どちらかを採用
+  // const { data, error } = await useFetch<Article>(
+  //   config.public.PUBLIC_BACKEND_URL + `article/${id}`
+  // );
+
+  // if (!data.value || error.value) {
+  //   console.log(error.value?.message);
+  // }
+
+  // return data;
+};
